e29ef1746c684cc792d6618fc13cde06f43c0f90
sheng <atckmax0216@gmail.com>
2022-08-28 22:40:40 +0800

    Update 2022-08-28T14:40:30.531Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,9 +1,13 @@
+"use strict";
+
 function initSwiper() {
-  const swiper = new Swiper(".SeaScapes", {
+  var swiper = new Swiper(".SeaScapes", {
     /*  預設要顯示幾個卡片 */
     // slidesPerView: 1,
+
     /* 卡片元素的間隔 */
     // spaceBetween: 24,
+
     /* 斷點設定 */
     breakpoints: {
       /* 螢幕寬度大於等於 768px 時切換為 2 欄 */
@@ -15,12 +19,14 @@ function initSwiper() {
       // 992: {
       //   slidesPerView: 4
       // },
+
       /* 更小時都顯示 1 欄 */
     },
+
     /*自動播放 */
     autoplay: {
-      delay: 1500,
-    },
+      delay: 1500
+    }
     /*輪播按鈕*/
     // pagination: {
     //   /* 我想將分頁圓點綁在哪個 class */
@@ -28,8 +34,11 @@ function initSwiper() {
     //   /* 將輪播設定為可以點擊分頁圓點來切換圖片 */
     //   clickable: true
     // }
+
   });
 }
-
 /* 觸發自己定義的函式 */
+
+
 initSwiper();
+//# sourceMappingURL=all.js.map
